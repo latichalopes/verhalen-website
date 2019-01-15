a5f83b0c8c897292f52b6b7f6e6dbd6638aaa329
Laticha Lopes <33430645+latichalopes@users.noreply.github.com>
2019-01-15 15:31:04 +0100

    Add files via upload

---
+++ website/js/index.js
@@ -0,0 +1,7 @@
+  var zoekknop = document.querySelector('#zoeken');
+        var header = document.querySelector('body > section.zoekbalk');
+
+        zoekknop.addEventListener('click', function(){
+
+            header.classList.toggle('show-form')
+                                                    });
